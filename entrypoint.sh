aaede91554cbc94568462ca9a7d982eda1a55843
Tom Brightbill <tbrightbill@cloudflare.com>
2019-01-16 16:23:29 -0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 serverless create --template cloudflare-workers --path deploy-worker 
 
+echo 'ls-ing below'
+
+ls
+
 cd deploy-worker 
 
 serverless plugin install --name serverless-cloudflare-workers 
