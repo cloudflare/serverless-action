c8bca0127c269d18f3721b4ed755786ee95286dd
Tom Brightbill <tbrightbill@cloudflare.com>
2019-01-17 10:39:05 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-pwd
+echo 'ls below'
 ls
+echo 'ls -la below'
+ls -la
+echo 'mkdir below'
+mkdir deploy-worker
+
+echo 'post mkdir ls'
+
+ls
+
+cd deploy-worker
+
 serverless create --template cloudflare-workers 
 
-serverless plugin install --name serverless-cloudflare-workers 
+serverless plugin install --name serverless-cloudflare-workers
 
-npm install
-
-serverless deploy
