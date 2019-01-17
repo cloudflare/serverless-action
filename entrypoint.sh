9f4b372ded9bdb8606fdcd875fb37ddac4775327
Tom Brightbill <tbrightbill@cloudflare.com>
2019-01-17 14:56:34 -0800

    message

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,11 +16,16 @@ serverless create --template cloudflare-workers
 
 serverless plugin install --name serverless-cloudflare-workers
 
-rm -rf hello-world.js
+rm -rf helloWorld.js
 
 mv $GITHUB_WORKSPACE/*.{yml,js} $GITHUB_WORKSPACE/deploy-worker/
 
 echo 'ls within deploy-worker directory'
+
+ls
+
+cat helloWorld.js
+cat serverless.yml
 
 echo $CLOUDFLARE_ACCOUNT_ID
 echo $CLOUDFLARE_ZONE_ID
