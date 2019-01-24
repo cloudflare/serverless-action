4f15f4fc19d90e636cfab0db5348b7bea09f0071
Tom Brightbill <tbrightbill@cloudflare.com>
2019-01-24 12:26:21 -0800

    updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-echo 'ls below'
-ls
-echo 'ls -la below'
-ls -la
-echo 'mkdir below'
+
 mkdir deploy-worker
-
-echo 'post mkdir ls'
-
-ls
 
 cd deploy-worker
 
@@ -20,17 +12,5 @@ rm -rf helloWorld.js
 
 mv $GITHUB_WORKSPACE/*.{yml,js} $GITHUB_WORKSPACE/deploy-worker/
 
-echo 'ls within deploy-worker directory'
-
-ls
-
-cat helloWorld.js
-cat serverless.yml
-
-echo $CLOUDFLARE_ACCOUNT_ID
-echo $CLOUDFLARE_ZONE_ID
-echo $CLOUDFLARE_AUTH_EMAIL
-echo $CLOUDFLARE_AUTH_KEY
-
-serverless deploy --verbose
+sls deploy
 
